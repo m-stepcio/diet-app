bab3dfc812aacc4d5a4b2ea88d9e82b229cf8923
m-stepcio <m.stepien1606@protonmail.com>
2026-09-08 20:11:14 +0200

    start working on consumer

---
+++ nutrition/src/main/java/com/diet/app/consumer/NutritionMessageConsumer.java
@@ -0,0 +1,17 @@
+package com.diet.app.consumer;
+
+import com.diet.app.dto.Nutrition;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class NutritionMessageConsumer {
+    private final static String NUTRITION_MESSAGE = "nutrition";
+    private final static String CONsuMER_GROUP_ID = "nutrition-app";
+    @KafkaListener(topics = NUTRITION_MESSAGE, groupId = CONsuMER_GROUP_ID)
+    public void listen(Nutrition nutrition){
+        log.info("Recive new message {}", nutrition);
+    }
+}
